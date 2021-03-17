34b2cb74fbad23647c080d8a486a1e51bb0f453c
Joe Grigg <account@joegrigg.com>
2021-03-17 07:23:57 +0000

    Setup js framework for uploading attachments

--- src/js/controllers/page_form_controller.ts
+++ src/js/controllers/page_form_controller.ts
@@ -6,6 +6,7 @@ import Notyf from '../notyf';
 
 export default class extends Controller {
   private save = debounce(this.handleSave, 1000);
+  private params = new URLSearchParams(document.location.search.substring(1));
   declare readonly formTarget: HTMLFormElement;
 
   static get targets(): Array<string> {
@@ -20,7 +21,7 @@ export default class extends Controller {
   setupDatePicker(): void {
     flatpickr('.datetime', {
       enableTime: true,
-      dateFormat: 'Y-m-d H:i:S', // THIS HAS SOME PROBLEMS (Time not being saved)
+      dateFormat: 'Y-m-d H:i:S', // TODO: THIS HAS SOME PROBLEMS (Time not being saved)
       altFormat: 'l, j F Y @ G:i K',
       altInput: true,
       position: 'auto'
@@ -30,33 +31,48 @@ export default class extends Controller {
   addTrixListeners(): void {
     let pageEditor = document.querySelector('#page-trix-editor');
     if (pageEditor == null) return;
-    pageEditor.addEventListener('trix-change', () => {
-      this.handleChange();
-    });
-    pageEditor.addEventListener('trix-attachment-add', (e) => {
-      console.log(e.attachment); // ATTACHMENT DOES EXIST BUT IS NOT RECOGNISED
-    });
+    pageEditor.addEventListener('trix-change', () => { this.handleChange() });
+    pageEditor.addEventListener('trix-attachment-add', (e: any) => { this.handleAttachment(e) });
   }
 
   handleSave(): void {
-    let params = new URLSearchParams(document.location.search.substring(1));
     let formData = new FormData(this.formTarget);
     Axios({
       method: 'post',
-      url: `/pages/${params.get('p')}`,
+      url: `/pages/${this.params.get('p')}`,
       data: formData,
       headers: { 'Content-Type': 'multipart/form-data' }
     })
-    .then(res => {
+    .then(_res => {
       Notyf.success('Saved!');
     })
-    .catch(err => {
+    .catch(_err => {
       Notyf.success('Failed to save...');
     })
-    
   }
 
   handleChange(): void {
     this.save();
+  }
+
+  handleAttachment(e: any): void { // TODO: EVENT NEEDS TO BE MADE THE CORRECT TYPE
+    if (e.attachment.file) this.uploadAttachment(e.attachment.file);
+  }
+
+  uploadAttachment(file: any) {
+    let formData = new FormData();
+    formData.append('file', file);
+    Axios({
+      method: 'post',
+      url: `/attachments/${this.params.get('p')}`,
+      data: formData,
+      headers: { 'Content-Type': 'multipart/form-data' }
+    })
+    .then(res => {
+      console.log('Res: ', res);
+    })
+    .catch(err => {
+      console.log('Err: ', err);
+    })
   }
 }
