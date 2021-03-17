e65ac299a7061f86a205054d1bd73a0aa42faebe
Joe Grigg <account@joegrigg.com>
2021-03-17 21:17:33 +0000

    Implemented temp solution to handling attachment upload

--- src/actions/attachments/create.cr
+++ src/actions/attachments/create.cr
@@ -1,7 +1,9 @@
 class Attachments::Create < ApiAction
   post "/attachments/:page_id" do
     page = PageQuery.new.find(page_id)
-    SaveAttachment.create(params, page_id: page.id) do |op, attachment|
+    SaveAttachment.create(file: params.nested_file("attachment")["file"], page_id: page.id) do |op, attachment|
+    # raise params.nested_file?("attachment").inspect
+    # SaveAttachment.create(params, page_id: page.id) do |op, attachment|
       if attachment
         plain_text "Uploaded"
       else
