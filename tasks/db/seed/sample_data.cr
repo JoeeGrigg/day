920f14b01d0f448c79b71bf2a23e25134588421c
Joe Grigg <account@joegrigg.com>
2021-03-26 07:21:29 +0000

    Added sample seed data

--- tasks/db/seed/sample_data.cr
+++ tasks/db/seed/sample_data.cr
@@ -8,21 +8,21 @@ class Db::Seed::SampleData < LuckyCli::Task
   summary "Add sample database records helpful for development"
 
   def call
-    # Using a Avram::Factory:
-    #
-    # Use the defaults, but override just the email
-    # UserFactory.create &.email("me@example.com")
+    months = 24
+    content = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quisque sagittis purus sit amet volutpat. Interdum posuere lorem ipsum dolor. Diam maecenas sed enim ut sem viverra. Dignissim suspendisse in est ante in. Elit sed vulputate mi sit amet mauris. Posuere urna nec tincidunt praesent semper. Mattis vulputate enim nulla aliquet. Viverra orci sagittis eu volutpat odio facilisis. Aliquam vestibulum morbi blandit cursus. Quis varius quam quisque id diam vel. Facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat. Elementum tempus egestas sed sed risus pretium quam vulputate. Facilisis volutpat est velit egestas dui id ornare. Nec feugiat in fermentum posuere urna nec tincidunt praesent semper.</p>"
+    
+    (0..months).each do |month|
+      pages = Random.new.rand(5)
+      (0..pages).each do |page|
+        days = Random.new.rand(27)
+        SavePage.create!(
+          name: "Generated Page #{UUID.random}",
+          content: content,
+          timestamp: (Time.utc - month.months).at_beginning_of_month + days.days
+        )
+      end
+    end
 
-    # Using a SaveOperation:
-    #
-    # SaveUser.create!(email: "me@example.com", name: "Jane")
-    #
-    # You likely want to be able to run this file more than once. To do that,
-    # only create the record if it doesn't exist yet:
-    #
-    # unless UserQuery.new.email("me@example.com").first?
-    #  SaveUser.create!(email: "me@example.com", name: "Jane")
-    # end
     puts "Done adding sample data"
   end
 end
