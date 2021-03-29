b7471207c6a36a332b822193d35946dea0cf91bb
Joe Grigg <account@joegrigg.com>
2021-03-29 08:25:05 +0100

    Added notes about potential fix to upload url issue

--- config/shrine.cr
+++ config/shrine.cr
@@ -1,3 +1,45 @@
+# class Shrine
+#   module Storage
+#     class S3 < Storage::Base
+#       def url(id : String, **options) : String
+#         presigned_options = Awscr::S3::Presigned::Url::Options.new(
+#           aws_access_key: client.@aws_access_key,
+#           aws_secret_key: client.@aws_secret_key,
+#           region: client.@region,
+#           object: "/#{object_key(id)}",
+#           bucket: bucket,
+#           host_name: client.@endpoint
+#         )
+
+#         url = Awscr::S3::Presigned::Url.new(presigned_options)
+#         url.for(:get)
+#       end
+#     end
+#   end
+# end
+
+# module Awscr
+#   module S3
+#     module Presigned
+#       class Url
+#         def for(method : Symbol)
+#           raise S3::Exception.new("unsupported method #{method}") unless allowed_methods.includes?(method)
+
+#           request = build_request(method.to_s.upcase)
+
+#           @options.additional_options.each do |k, v|
+#             request.query_params.add(k, v)
+#           end
+
+#           presign_request(request)
+
+#           "#{request.headers["Host"]}#{request.resource}"
+#         end
+#       end
+#     end
+#   end
+# end
+
 client = Awscr::S3::Client.new(
   "minio",
   "minioadmin",
