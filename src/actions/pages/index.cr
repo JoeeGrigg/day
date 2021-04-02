8250b86ef82e9d76c7cb4ab6617838cd93c7d32d
Joe Grigg <account@joegrigg.com>
2021-04-02 16:02:33 +0100

    Updated to only load pages for the logged in user

--- src/actions/pages/index.cr
+++ src/actions/pages/index.cr
@@ -3,8 +3,8 @@ class Pages::Index < BrowserAction
   param p : UUID?
 
   get "/pages" do
-    pages = PageQuery.new.timestamp.desc_order
-    page = p ? PageQuery.new.find(p.not_nil!) : nil
+    pages = PageQuery.new.user_id(current_user.id).timestamp.desc_order
+    page = p ? PageQuery.new.user_id(current_user.id).find(p.not_nil!) : nil
     html IndexPage,
          pages: pages,
          page: page,
