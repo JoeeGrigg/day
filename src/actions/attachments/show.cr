63089bcefcf1182ce9a2a8f3305d0df8fdd43915
Joe Grigg <account@joegrigg.com>
2021-04-10 12:19:38 +0100

    Fixed attachment rendering

--- src/actions/attachments/show.cr
+++ src/actions/attachments/show.cr
@@ -1,6 +1,6 @@
 class Attachments::Show < BrowserAction
   get "/attachments/:page_id/:attachment_id" do
     attachment = AttachmentQuery.new.page_id(page_id).find(attachment_id)
-    html ShowPage, url: attachment.url
+    redirect attachment.url
   end
 end
--- src/pages/attachments/show_page.cr
+++
@@ -1,9 +0,0 @@
-class Attachments::ShowPage
-  include Lucky::HTMLPage
-
-  needs url : String
-
-  def render
-    img src: url
-  end
-end
