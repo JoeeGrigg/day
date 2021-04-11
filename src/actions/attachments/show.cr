204f66577e186dcf2794320a24a57321dd3511f3
Joe Grigg <account@joegrigg.com>
2021-04-11 13:55:50 +0100

    Cleaned up attachment actions with helper method

--- src/actions/attachments/show.cr
+++ src/actions/attachments/show.cr
@@ -1,12 +1,10 @@
 class Attachments::Show < BrowserAction
   get "/attachments/:page_id/:attachment_id" do
-    attachment = AttachmentQuery.new
-      .where_page(
-        PageQuery.new
-          .user_id(current_user.id)
-          .id(page_id)
-      )
-      .find(attachment_id)
+    attachment = AttachmentQuery.new.for_action(
+      current_user.id,
+      attachment_id,
+      page_id
+    )
     redirect attachment.url
   end
 end
--- src/queries/attachment_query.cr
+++ src/queries/attachment_query.cr
@@ -1,3 +1,10 @@
 class AttachmentQuery < Attachment::BaseQuery
   include Avram::SoftDelete::Query
+
+  alias ForActionID = String | UUID
+  def for_action(user_id : ForActionID, id : ForActionID, page_id : ForActionID) : Attachment
+    page_query = PageQuery.new.user_id(user_id).id(page_id)
+
+    where_page(page_query).find(id)
+  end
 end
